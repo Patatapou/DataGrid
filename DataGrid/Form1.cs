e770395e46151782faad38f78518b9723a5d02f5
Patatapou <migel2013@outlook.com>
2025-02-19 11:23:32 -0600

    Funciones para separar sexo y estado

--- DataGrid/Form1.cs
+++ DataGrid/Form1.cs
@@ -29,13 +29,65 @@ namespace DataGrid
                 string year = curp[4].ToString() + curp[5].ToString();
                 string mes = curp[6].ToString() + curp[7].ToString();
                 string dia = curp[8].ToString() + curp[9].ToString();
+
+                string sexo = curp[10].ToString();
+                string estado = curp[11].ToString() + curp[12].ToString();
                 DateTime fecha = DateTime.Parse(dia+"/"+mes+"/"+year);
 
-                MessageBox.Show("La fecha de nacimiento es: " + fecha.ToString("dd/MM/yyyy"));
+                MessageBox.Show("La fecha de nacimiento es: " + fecha.ToString("dd/MM/yyyy") + "\nEl sexo es: " + Sexo(sexo) + "\nEl estado es: " + Estado(estado));
             } 
             else 
             {
                 MessageBox.Show("Ingrese una CURP valida");
+            }
+        }
+
+        public string Sexo(string sexo)
+        {
+            if (sexo == "H")
+            {
+                return "Masculino";
+            }
+            else
+            {
+                return "Femenino";
+            }
+        }
+        public string Estado(string estado)
+        {
+            switch (estado)
+            {
+                case "AG": return "Aguascalientes"; break;
+                case "BC": return "Baja California"; break;
+                case "BS": return "Baja California Sur"; break;
+                case "CC": return "Campeche"; break;
+                case "CS": return "Chiapas"; break;
+                case "CH": return "Chihuahua"; break;
+                case "CM": return "Coahuila"; break;
+                case "DG": return "Durango"; break;
+                case "GT": return "Guanajuato"; break;
+                case "GR": return "Guerrero"; break;
+                case "HG": return "Hidalgo"; break;
+                case "JC": return "Jalisco"; break;
+                case "MC": return "México"; break;
+                case "MN": return "Michoacán"; break;
+                case "MS": return "Morelos"; break;
+                case "NT": return "Nayarit"; break;
+                case "NL": return "Nuevo León"; break;
+                case "OC": return "Oaxaca"; break;
+                case "PL": return "Puebla"; break;
+                case "QT": return "Querétaro"; break;
+                case "QR": return "Quintana Roo"; break;
+                case "SP": return "San Luis Potosí"; break;
+                case "SL": return "Sinaloa"; break;
+                case "SR": return "Sonora"; break;
+                case "TC": return "Tabasco"; break;
+                case "TS": return "Tamaulipas"; break;
+                case "TL": return "Tlaxcala"; break;
+                case "VZ": return "Veracruz"; break;
+                case "YN": return "Yucatán"; break;
+                case "ZS": return "Zacatecas"; break;
+                default: return "Abreviatura no reconocida"; break;
             }
         }
     }
